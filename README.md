8362d99c59d60ea019eb6d2feb15e079adb2d46b
njuneidi <njuneidi@bethlehem.edu>
2026-04-16 08:34:29 +0200

    First Flutt App messege

---
+++ README.md
@@ -0,0 +1,3 @@
+# form_demo_copy
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/form_demo_copy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.form_demo_copy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
